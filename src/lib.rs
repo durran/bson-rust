45689b59e86305aa5c52fa029723541ff2776359
Durran Jordan <durran@gmail.com>
2017-02-02 08:36:38 +0100

    Setting up serialization behaviour

--- src/lib.rs
+++ src/lib.rs
@@ -1,6 +1,37 @@
-#[cfg(test)]
-mod tests {
-    #[test]
-    fn it_works() {
+extern crate byteorder;
+
+use byteorder::{LittleEndian, WriteBytesExt};
+use std::io::{Result, Write};
+
+/// All types that can be converted to BSON must implement the
+/// `Serializable` trait.
+trait Serializable {
+
+    /// Serializes the object to BSON. Implementation should write the raw
+    /// bytes to the provided writer.
+    ///
+    /// # Parameters
+    /// - `writer` - The `Writer` to write to.
+    ///
+    /// # Returns
+    /// - `result` - The `Result` object.
+    fn to_bson<W: Write + ?Sized>(&self, writer: &mut W) -> Result<()>;
+}
+
+impl Serializable for String {
+
+    /// Serializes the object to BSON. Implementation should write the raw
+    /// bytes to the provided writer.
+    ///
+    /// # Parameters
+    /// - `writer` - The `Writer` to write to.
+    ///
+    /// # Returns
+    /// - `result` - The `Result` object.
+    fn to_bson<W: Write + ?Sized>(&self, writer: &mut W) -> Result<()> {
+        writer.write_i32::<LittleEndian>(self.len() as i32 + 1);
+        writer.write_all(self.as_bytes());
+        writer.write_u8(0);
+        Ok(())
     }
 }
