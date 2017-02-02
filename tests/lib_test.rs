a6340b01503d9212536efcc97b16479e028e7e96
Durran Jordan <durran@gmail.com>
2017-02-02 09:11:39 +0100

    Flushing out more test patterns

--- tests/lib_test.rs
+++ tests/lib_test.rs
@@ -7,21 +7,25 @@ extern crate bson;
 
 use bson::*;
 use expectest::prelude::*;
-use std::io::{Cursor, Result};
+use std::io::Cursor;
 
 describe! lib_test {
     describe! string_test {
-        describe! success {
-            before_each {
-                let mut writer = Cursor::new(vec![]);
-                let result = "testing".to_string().to_bson(&mut writer);
-            }
+        describe! serialization {
+            describe! success {
+                before_each {
+                    let mut writer = Cursor::new(vec![]);
+                    let expected = vec![8, 0, 0, 0, 116, 101, 115, 116, 105, 110, 103, 0];
+                    let result = "testing".to_string().to_bson(&mut writer);
+                }
 
-            it "writes the bytes to the writer" {
-            }
+                it "returns an ok result" {
+                    expect!(result).to(be_ok());
+                }
 
-            it "returns an ok result" {
-                expect!(result).to(be_ok());
+                it "writes the bytes to the writer" {
+                    expect!(writer.into_inner()).to(be_equal_to(expected));
+                }
             }
         }
     }
