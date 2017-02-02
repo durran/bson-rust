ff6f29c521f4200d6314be6f06165df0868b1248
Durran Jordan <durran@gmail.com>
2017-02-02 14:27:03 +0100

    Move string specific serialization into own module

---
+++ src/string.rs
@@ -0,0 +1,44 @@
+extern crate byteorder;
+
+use byteorder::{LittleEndian, ReadBytesExt, WriteBytesExt};
+use std::io::{Read, Result, Write};
+use super::*;
+
+/// Implements BSON serialization for `String` types.
+impl Serializable for String {
+
+    /// Serializes the object to BSON. Implementation should write the raw
+    /// bytes to the provided writer.
+    ///
+    /// # Parameters
+    /// - `writer` - The `Writer` to write to.
+    ///
+    /// # Returns
+    /// - `result` - The `Result` object.
+    fn to_bson<W: Write + ?Sized>(&self, writer: &mut W) -> Result<()> {
+        writer.write_i32::<LittleEndian>(self.len() as i32 + 1);
+        writer.write_all(self.as_bytes());
+        writer.write_u8(0);
+        Ok(())
+    }
+}
+
+/// Implements BSON deserializtion for `String` types.
+impl Deserializable<String> for String {
+
+    /// Deserializes the BSON into an object, reading the bytes from the
+    /// reader.
+    ///
+    /// # Parameters
+    /// - `reader` - The `Reader` to read from.
+    ///
+    /// # Returns
+    /// - `Result<String>` - The `Result` object with the string value if ok.
+    fn from_bson<R: Read + ?Sized>(reader: &mut R) -> Result<String> {
+        let length = reader.read_i32::<LittleEndian>().unwrap();
+        let mut string = String::with_capacity(length as usize - 1);
+        reader.take(length as u64 - 1).read_to_string(&mut string);
+        reader.read_u8();
+        Ok(string)
+    }
+}
---
+++ tests/string_test.rs
@@ -0,0 +1,48 @@
+#![feature(plugin)]
+#![cfg_attr(test, plugin(stainless))]
+#[macro_use(expect)]
+
+extern crate expectest;
+extern crate bson;
+
+use bson::*;
+use expectest::prelude::*;
+use std::io::Cursor;
+
+describe! string_test {
+    describe! serialization {
+        describe! success {
+            before_each {
+                let mut writer = Cursor::new(vec![]);
+                let expected = vec![8, 0, 0, 0, 116, 101, 115, 116, 105, 110, 103, 0];
+                let result = "testing".to_string().to_bson(&mut writer);
+            }
+
+            it "returns an ok result" {
+                expect!(result).to(be_ok());
+            }
+
+            it "writes the bytes to the writer" {
+                expect!(writer.into_inner()).to(be_equal_to(expected));
+            }
+        }
+    }
+
+    describe! deserialization {
+        describe! success {
+            before_each {
+                let bytes = vec![8, 0, 0, 0, 116, 101, 115, 116, 105, 110, 103, 0];
+                let mut reader = Cursor::new(bytes);
+                let result = String::from_bson(&mut reader);
+            }
+
+            it "returns an ok result" {
+                expect!(result).to(be_ok());
+            }
+
+            it "reads the bytes from the reader" {
+                expect!(result).to(be_ok().value("testing"));
+            }
+        }
+    }
+}
