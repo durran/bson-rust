10e8bd46189bb0475b8677771293b410036ed217
Durran Jordan <durran@gmail.com>
2017-02-08 12:11:37 +1100

    Adding doc get tests

--- tests/document_test.rs
+++ tests/document_test.rs
@@ -9,6 +9,34 @@ use bson::{Bson, Document};
 use expectest::prelude::*;
 
 describe! document_test {
+    describe! get {
+        describe! when_the_key_does_not_exist {
+            before_each {
+                let mut document = Document::new();
+            }
+
+            it "returns none" {
+                expect!(document.get("test")).to(be_none());
+            }
+        }
+
+        describe! when_the_key_exists {
+            before_each {
+                let mut document = Document::new();
+                let key = "test".to_string();
+                let string = "value".to_string();
+                let value = Bson::String(string);
+                let option = document.insert(key, value);
+            }
+
+            it "returns the some option" {
+                expect!(document.get("test")).to(
+                    be_equal_to(Some(&Bson::String("value".to_string())))
+                );
+            }
+        }
+    }
+
     describe! insert {
         describe! when_the_key_does_not_exist {
             before_each {
